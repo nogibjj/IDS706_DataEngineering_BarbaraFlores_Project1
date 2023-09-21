d66becb193453785727d0d3d9e9eafed46a159c3
Barbara Flores Rios <143648839+BarbaraPFloresRios@users.noreply.github.com>
2023-09-21 16:53:16 +0000

    add histogram

--- script.py
+++ script.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 from mylib.calculator import mean_variable, median_variable, count_variable
 
 def aggregate_statistics(path, variable_list):
@@ -6,6 +8,15 @@ def aggregate_statistics(path, variable_list):
         print(f"The median of variable {i} is {round(median_variable(path, i))}.")
         print(f"The count of variable {i} is {round(count_variable(path, i))}.")
 
+def hist_plot(path,variable):
+    df = pd.read_csv(path) 
+    plt.hist(df[[variable]], bins=10,)
+    plt.xlabel(variable)
+    plt.ylabel('frequency')
+    plt.title('Histogram of {0} per job posting'.format(variable))
+    plt.savefig("{}.png".format(variable))
+
 
 if __name__ == "__main__":
     aggregate_statistics('LinkedInTechJobsDataset.csv', ["Total_applicants", "Employee_count", "LinkedIn_Followers"])
+    hist_plot("LinkedInTechJobsDataset.csv", "Total_applicants")
