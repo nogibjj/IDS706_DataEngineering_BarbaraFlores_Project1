06a275b2806d075b1ae327388000e2db199cff4c
Barbara Flores Rios <143648839+BarbaraPFloresRios@users.noreply.github.com>
2023-09-21 23:32:23 +0000

    changes

--- test_script.py
+++ test_script.py
@@ -1,11 +1,22 @@
 #from script import aggregate_statistics
 import requests
 
-def check_github_file_existence(owner, repo, path):
+
+def test_aggregate_statistics(path):
+    df = pd.read_csv(path)
+    if aggregate_statistics(path) is not None:
+        return True
+    else:
+        raise Exception(
+            f"Error when checking the existence of the result: {response.status_code}"
+            )
+
+def test_github_file_existence(owner, repo, path):
     url = f"https://raw.githubusercontent.com/{owner}/{repo}/main/{path}"
 
     response = requests.get(url)
     assert  response.status_code == 200
+
 
 
 if __name__ == "__main__":
@@ -14,7 +25,7 @@ if __name__ == "__main__":
     path1 = "Total_applicants.png"
     path2 = "Employee_count.png"
     path3 = "LinkedIn_Followers.png"
-    check_github_file_existence(owner, repo, path1)
-    check_github_file_existence(owner, repo, path2)
-    check_github_file_existence(owner, repo, path3)
+    test_github_file_existence(owner, repo, path1)
+    test_github_file_existence(owner, repo, path2)
+    test_github_file_existence(owner, repo, path3)
  
