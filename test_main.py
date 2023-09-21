0d22ff9e87440fcc5c23fd3ae22841a1b6efb379
Barbara Flores Rios <143648839+BarbaraPFloresRios@users.noreply.github.com>
2023-09-21 16:01:20 +0000

    changes in tet

--- test_main.py
+++ test_main.py
@@ -1,11 +1,17 @@
 import pandas as pd
-import main
 from mylib.calculator import mean_variable, median_variable, count_variable
 
     
 #if __name__ == "__main__":
-assert mean_variable("data_test.csv", "age") == 32
-assert median_variable("data_test.csv", "age") == 30
-assert count_variable("data_test.csv", "age") == 5
+
+def test_mean():
+    assert mean_variable("data_test.csv", "age") == 32
+
+def test_median():
+    assert median_variable("data_test.csv", "age") == 30
+
+def test_count_variable():
+    assert count_variable("data_test.csv", "age") == 5
+
 
 
