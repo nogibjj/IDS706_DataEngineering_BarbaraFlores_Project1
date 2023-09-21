76e98359146af5988d4383fd92c814886ebd6688
Barbara Flores Rios <143648839+BarbaraPFloresRios@users.noreply.github.com>
2023-09-21 11:21:58 +0000

    changes in requirements

--- main.py
+++ main.py
@@ -2,17 +2,15 @@
 Main cli or app entry point
 """
 
-from mylib.calculator import add
-import click
-
-
-@click.command("add")
-@click.argument("a", type=int)
-@click.argument("b", type=int)
-def add_cli(a, b):
-    click.echo(add(a, b))
+from mylib.calculator import mean_variable, median_variable, count_variable
 
 
 if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
-    add_cli()
+    for i in ["Total_applicants", "Employee_count", "LinkedIn_Followers"]:
+        print(f"The mean of variable {i} is {round(mean_variable('LinkedInTechJobsDataset.csv', i))}.")
+    print()
+    for i in ["Total_applicants", "Employee_count", "LinkedIn_Followers"]:
+        print(f"The median of variable {i} is {round(median_variable('LinkedInTechJobsDataset.csv', i))}.")
+    print()
+    for i in ["Total_applicants", "Employee_count", "LinkedIn_Followers"]:
+        print(f"The count of variable {i} is {round(count_variable('LinkedInTechJobsDataset.csv', i))}.")
--- mylib/calculator.py
+++ mylib/calculator.py
@@ -1,19 +1,18 @@
 """
 Calculations library
 """
+# imports
+import pandas as pd
+import matplotlib.pyplot as plt
 
+def mean_variable(path, variable):
+    df = pd.read_csv(path)
+    return df[variable].mean()
 
-def add(a, b):
-    return a + b
+def median_variable(path, variable):
+    df = pd.read_csv(path)
+    return df[variable].median()
 
-
-def subtract(a, b):
-    return a - b
-
-
-def multiply(a, b):
-    return a * b
-
-
-def divide(a, b):
-    return a / b
+def count_variable(path, variable):
+    df = pd.read_csv(path)
+    return df[variable].sum() / df[variable].mean() 
