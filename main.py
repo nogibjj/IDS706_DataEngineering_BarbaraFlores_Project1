c064804326adde93a95420b03de96be6126c893e
Barbara Flores Rios <143648839+BarbaraPFloresRios@users.noreply.github.com>
2023-09-21 12:30:29 +0000

    lalal

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from mylib.calculator import mean_variable, median_variable, count_variable
 
 def aggregate_statistics(path, variable_list):
-     for i in variable_list:
+    for i in variable_list:
         print(f"The mean of variable {i} is {round(mean_variable('LinkedInTechJobsDataset.csv', i))}.")
         print(f"The median of variable {i} is {round(median_variable('LinkedInTechJobsDataset.csv', i))}.")
         print(f"The count of variable {i} is {round(count_variable('LinkedInTechJobsDataset.csv', i))}.")
