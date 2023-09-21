32995ea21e0f8de9d7244444f3abdac60231000e
Barbara Flores Rios <143648839+BarbaraPFloresRios@users.noreply.github.com>
2023-09-21 12:45:08 +0000

    changes

--- test_main.py
+++ test_main.py
@@ -3,9 +3,9 @@ import main
 from mylib.calculator import mean_variable, median_variable, count_variable
 
     
-if __name__ == "__main__":
-    assert mean_variable("data_test.csv", "age") == 32
-    assert median_variable("data_test.csv", "age") == 30
-    assert count_variable("data_test.csv", "age") == 5
+#if __name__ == "__main__":
+assert mean_variable("data_test.csv", "age") == 32
+assert median_variable("data_test.csv", "age") == 30
+assert count_variable("data_test.csv", "age") == 5
 
 
