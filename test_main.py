aff0b501b9a00afe6b8cc12082936b25ec1fbd9f
Barbara Flores Rios <143648839+BarbaraPFloresRios@users.noreply.github.com>
2023-09-21 11:51:50 +0000

    changes in test

--- test_main.py
+++ test_main.py
@@ -2,9 +2,39 @@
 Test goes here
 
 """
+import pandas as pd
+from mylib.calculator import mean_variable, median_variable, count_variable
 
-from mylib.calculator import add
+#def test_add():
+ #   assert add(1, 2) == 3
+def check_mean_variable(path, variable):
+    df = pd.read_csv(path)
+    if mean_variable(path, variable) ==  df[variable].mean():
+        return True
+    else:
+        raise Exception(
+            "houston we have a problem"   
+        )   
+    
+def check_median_variable(path, variable):
+    df = pd.read_csv(path)
+    if median_variable(path, variable) ==  df[variable].median():
+        return True
+    else:
+        raise Exception(
+             "houston we have a problem"    
+        )      
 
-
-def test_add():
-    assert add(1, 2) == 3
+def check_count_variable(path, variable):
+    df = pd.read_csv(path)
+    if count_variable(path, variable) == df[variable].sum() / df[variable].mean() :
+        return True
+    else:
+        raise Exception(
+             "houston we have a problem"   
+        )      
+    
+if __name__ == "__main__":
+    assert check_mean_variable("LinkedInTechJobsDataset.csv","Total_applicants")
+    assert check_median_variable("LinkedInTechJobsDataset.csv","Total_applicants")
+    assert check_count_variable("LinkedInTechJobsDataset.csv","Total_applicants")
