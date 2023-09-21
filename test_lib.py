08d35fba8e3c6d3cc4673c7f1f6fe91095abd499
Barbara Flores Rios <143648839+BarbaraPFloresRios@users.noreply.github.com>
2023-09-21 12:26:12 -0400

    Create test_lib.py

---
+++ test_lib.py
@@ -0,0 +1,11 @@
+from mylib.calculator import mean_variable, median_variable, count_variable
+
+
+def test_mean():
+    assert mean_variable("data_test.csv", "age") == 32
+
+def test_median():
+    assert median_variable("data_test.csv", "age") == 30
+
+def test_count_variable():
+    assert count_variable("data_test.csv", "age") == 5
