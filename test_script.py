862216d2b066ba644091c87198fde3d480ea3fe7
Barbara Flores Rios <143648839+BarbaraPFloresRios@users.noreply.github.com>
2023-09-22 02:09:23 +0000

    changes in test

--- test_script.py
+++ test_script.py
@@ -1,20 +1,19 @@
 #from script import aggregate_statistics
 import requests
 
-def test_github_file_existence(owner, repo, path):
-    url = f"https://raw.githubusercontent.com/{owner}/{repo}/main/{path}"
+def test_github_file_existence(repo, path):
+    url = f"https://raw.githubusercontent.com/{repo}/main/{path}"
 
     response = requests.get(url)
     assert  response.status_code == 200
 
 
 if __name__ == "__main__":
-    owner = "nogibjj"
-    repo = "IDS706_DataEngineering_BarbaraFlores_Project1"
+    repo = "nogibjj/IDS706_DataEngineering_BarbaraFlores_Project1"
     path1 = "Total_applicants.png"
     path2 = "Employee_count.png"
     path3 = "LinkedIn_Followers.png"
-    test_github_file_existence(owner, repo, path1)
-    test_github_file_existence(owner, repo, path2)
-    test_github_file_existence(owner, repo, path3)
+    test_github_file_existence(repo, path1)
+    test_github_file_existence(repo, path2)
+    test_github_file_existence(repo, path3)
  
